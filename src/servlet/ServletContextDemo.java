aae95efa4185a53583b8be3713a63e3cdbeaa497
MSER <gaoyong0520@outlook.com>
2018-04-11 23:41:21 +0800

    ServletContext

--- src/servlet/ServletContextDemo.java
+++ src/servlet/ServletContextDemo.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ServletContextDemo")
 public class ServletContextDemo extends HttpServlet {
     private ServletContext context;
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

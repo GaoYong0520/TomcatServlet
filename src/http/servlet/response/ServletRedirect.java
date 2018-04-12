182a772ace294027be6511b2219bcd8232a9b470
MSER <gaoyong0520@outlook.com>
2018-04-12 23:17:18 +0800

    Servlet重定向

--- src/http/servlet/response/ServletRedirect.java
+++ src/http/servlet/response/ServletRedirect.java
@@ -7,5 +7,6 @@ public class ServletRedirect extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
         System.out.println("Hello, source message");
+        response.sendRedirect("https://www.baidu.com");
     }
 }

3a2dceed7abded058c4c0754dc47d0f7c241521c
MSER <gaoyong0520@outlook.com>
2018-04-12 23:16:25 +0800

    ServletSession

---
+++ src/http/servlet/ServletGetSession.java
@@ -0,0 +1,20 @@
+package http.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class ServletGetSession extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+        System.out.println("get session:" + name);
+    }
+}
---
+++ src/http/servlet/SessionDemo.java
@@ -0,0 +1,22 @@
+package http.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class SessionDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        HttpSession session = request.getSession();
+        session.setAttribute("name",name);
+        String myname = (String) session.getAttribute("name");
+        System.out.println("session:" + myname);
+    }
+}
